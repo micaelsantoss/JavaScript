d3ed851185ceedfbe4526a95a332024c2ca1a471
Micael Santos <154888086+micaelsantoss@users.noreply.github.com>
2024-02-18 15:26:53 -0300

    aprendendo condição js

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var vel= 59.5
+console.log (`A sua velocidade foi de ${vel}km/h`)
+if (vel > 60) {
+    console.log (`Você passou acima da velocidade máxima permitida, MULTADO!`)    
+} else {
+    console.log (`Você passou abaixo da velocidade máxima permitida, PARABÉNS!`)
+}
+console.log (`Boa viagem!!`)
