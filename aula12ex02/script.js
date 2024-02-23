bfeb64058e87494b3d6ee2e5603c6edfbfaff00b
Micael Santos <154888086+micaelsantoss@users.noreply.github.com>
2024-02-23 13:37:47 -0300

    finalizando exercício de perfil

--- aula12ex02/script.js
+++ aula12ex02/script.js
@@ -6,20 +6,44 @@ function carregar(){
     var sexo = window.document.getElementsByName('rads')
     var calc = anoa - ano
     var res = window.document.getElementById('res')
-    var genero = ''
-        
+    var genero = '' //variavel sem informação
+    var img = document.createElement('img') //criando elemento de imagem
+    img.setAttribute('id', 'foto') //atribuindo id para a imagem   
 
-    if (ano.valueOf.length == 0){
-        window.alert ('Erro, Preencha os dados e tente novamente!')
-    } else{
-        
-    }
-        if (sexo[0].checked){
-            genero = 'Homem'
-        } else {
-            genero = 'Mulher' 
+    if (ano == 0 || ano > anoa){
+        window.alert ('Erro, Preencha os dados e tente novamente!')} else{
+            if (sexo[0].checked){//verificando qual opção foi marcada
+                genero = 'Homem' //colocando informação na variavel "genero"
+                if (calc >= 0 && calc < 10){
+                    //criança 
+                    img.setAttribute('src', 'criancah.jpg')
+                } else if(calc < 20){
+                    //jovem
+                    img.setAttribute('src', 'adolecenteh.jpg')         
+                } else if (calc < 50){
+                    //adulto
+                    img.setAttribute('src', 'adultoh.jpg')
+                } else if (calc >= 50){
+                    //idoso
+                    img.setAttribute('src', 'idosoh.jpg')
+                }
+            } else {
+                genero = 'Mulher' 
+                if (calc >= 0 && calc < 10){
+                    //criança
+                    img.setAttribute('src', 'criancam.jpg') 
+                } else if(calc < 20){
+                    //jovem
+                    img.setAttribute('src', 'adolecentem.jpg')
+                } else if (calc < 50){
+                    //adulta
+                    img.setAttribute('src', 'adultom.jpg')
+                } else if (calc >= 50){
+                    //idosa
+                    img.setAttribute('src', 'idosom.jpg')
+                }
+            } 
+            res.innerHTML = `Você tem ${calc} anos e é ${genero}`
+            res.appendChild(img)
         }
-        res.innerHTML = `Você tem ${calc} anos e é ${genero}`
-    
-   
 }
