c6f0736f1d0386b95586472956015b3559852d32
Micael Santos <154888086+micaelsantoss@users.noreply.github.com>
2024-03-04 14:29:38 -0300

    funções

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,1 +1,9 @@
- 
+var valores = [5,4,9,3,7,8,10,12]
+
+/*for (var pos=0 ; pos < valores.length ; pos++){
+    console.log (`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for (var pos in valores){
+   console.log (`A posição ${pos} tem o valor ${valores[pos]}`)
+}
